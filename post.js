56433d5b493df10c942a4e7aeb38cdfd4cd8484e
Stephen Hodgson <hodgson.designs@gmail.com>
2022-11-02 18:19:57 -0400

    just check in post script

--- post.js
+++ post.js
@@ -3,9 +3,14 @@ const github = require('@actions/github');
 
 const main = async () => {
     try {
-        // return license if pro/plus
-        console.log('-quit -batchmode -returnlicense -username name@example.com -password XXXXXXXXXXXXX');
-        // -quit -batchmode -returnlicense -username name@example.com -password XXXXXXXXXXXXX
+        var licenseType = core.getInput('license-type');
+
+        if(licenseType.toLowerCase().startsWith('pro')) {
+            // return license if pro/plus
+            console.log(`Returning ${licenseType} Unity License`);
+            console.log('-quit -batchmode -returnlicense -username name@example.com -password XXXXXXXXXXXXX');
+            // -quit -batchmode -returnlicense -username name@example.com -password XXXXXXXXXXXXX
+        }
     } catch (error) {
         core.setFailed(error.message);
     }
