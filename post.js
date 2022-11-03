be89375e33d7ea7a065c01eae7c50ebc06553f58
Stephen Hodgson <hodgson.designs@gmail.com>
2022-11-03 17:28:06 -0400

    update post script to return license on pro activation

--- post.js
+++ post.js
@@ -6,11 +6,27 @@ const main = async () => {
     try {
         var licenseType = core.getInput('license-type');
 
-        if(licenseType.toLowerCase().startsWith('pro')) {
+        if (licenseType.toLowerCase().startsWith('pro')) {
             // return license if pro/plus
             console.log(`Returning ${licenseType} Unity License`);
-            console.log('-quit -batchmode -returnlicense -username name@example.com -password XXXXXXXXXXXXX');
+
+            var username = core.getInput('username');
+
+            if (!username) {
+                throw Error('Missing username input');
+            }
+
+            var password = core.getInput('password');
+
+            if (!password) {
+                throw Error('Missing password input');
+            }
+
+            var pwsh = await io.which("pwsh", true);
+            var unity_action = path.resolve(__dirname, 'unity-action.ps1');
             // -quit -batchmode -returnlicense -username name@example.com -password XXXXXXXXXXXXX
+            var args = `quit -batchmode -returnlicense -username ${username} -password ${password}`;
+            await exec.exec(`"${pwsh}" -Command`, `${unity_action} -editorPath "${editorPath}" -additionalArgs ${args} -logName ReturnLicense`);
         }
     } catch (error) {
         core.setFailed(error.message);
