ceac7e98a465ef022a765ecd036a8f4cc6210a1a
Stephen Hodgson <hodgson.designs@gmail.com>
2022-11-06 20:48:22 -0500

    fix exports

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,8 @@
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 2281:
-/***/ ((__unused_webpack_module, __webpack_exports__, __nccwpck_require__) => {
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
-"use strict";
-__nccwpck_require__.r(__webpack_exports__);
-/* harmony export */ __nccwpck_require__.d(__webpack_exports__, {
-/* harmony export */   "Run": () => (/* binding */ Run)
-/* harmony export */ });
 const core = __nccwpck_require__(4191);
 const exec = __nccwpck_require__(6284);
 const io = __nccwpck_require__(156);
@@ -150,18 +145,13 @@ const findByExtension = async (dir, ext) => {
     return matchedFiles;
 };
 
+module.exports = { Run }
 
 /***/ }),
 
 /***/ 279:
-/***/ ((__unused_webpack_module, __webpack_exports__, __nccwpck_require__) => {
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
-"use strict";
-__nccwpck_require__.r(__webpack_exports__);
-/* harmony export */ __nccwpck_require__.d(__webpack_exports__, {
-/* harmony export */   "Run": () => (/* binding */ Run),
-/* harmony export */   "IsPost": () => (/* binding */ IsPost)
-/* harmony export */ });
 const core = __nccwpck_require__(4191);
 const exec = __nccwpck_require__(6284);
 const io = __nccwpck_require__(156);
@@ -208,7 +198,8 @@ async function Run() {
     }
 };
 
-const IsPost = !!core.getState('isPost');
+
+module.exports = { Run }
 
 /***/ }),
 
@@ -54106,34 +54097,6 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__nccwpck_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__nccwpck_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
@@ -54142,11 +54105,14 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
+const core = __nccwpck_require__(4191);
 const activate = __nccwpck_require__(2281);
 const deactivate = __nccwpck_require__(279);
 
+const IsPost = !!core.getState('isPost');
+
 const main = async () => {
-    if (!post.IsPost) {
+    if (!IsPost) {
         // activate license
         await activate.Run();
     } else {
