d74012f17fb06360a302c0c6356cd462648d009f
Stephen Hodgson <hodgson.designs@gmail.com>
2024-01-21 22:10:22 -0500

    revert some stuff

--- puppeteerrc.cjs
+++ puppeteerrc.cjs
@@ -4,6 +4,5 @@ const {join} = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
